ad596ddaaa9eb0338b41694efed9cc262bb17306
shroff09 <ankitnidhishroff9@gmail.com>
2020-10-13 18:26:20 +0530

    Updates

---
+++ precache-manifest.95d8550f36a5b9e55e2186c8a2823d1f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3090ea7faf55a77ab3dcbe01c91819a4",
+    "url": "/assessment/index.html"
+  },
+  {
+    "revision": "b8767be0883af7156183",
+    "url": "/assessment/static/css/2.438c4eac.chunk.css"
+  },
+  {
+    "revision": "deb404dba5ed7d8c1547",
+    "url": "/assessment/static/css/main.4a86df5e.chunk.css"
+  },
+  {
+    "revision": "b8767be0883af7156183",
+    "url": "/assessment/static/js/2.e1818d2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/assessment/static/js/2.e1818d2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deb404dba5ed7d8c1547",
+    "url": "/assessment/static/js/main.28a8b3dd.chunk.js"
+  },
+  {
+    "revision": "5863a9ca33cd02ac1461",
+    "url": "/assessment/static/js/runtime-main.1d0f82b5.js"
+  },
+  {
+    "revision": "4f54a7ee527de18a0f240975827c9e39",
+    "url": "/assessment/static/media/logo.4f54a7ee.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/assessment/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/assessment/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/assessment/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "413fcbb29a4d8b495ce88bcbbfd04801",
+    "url": "/assessment/static/media/sample1.413fcbb2.webp"
+  },
+  {
+    "revision": "4f6c55c239fb6c9632c45fc866dfb0f6",
+    "url": "/assessment/static/media/sample2.4f6c55c2.png"
+  },
+  {
+    "revision": "c8b11a06e506ddb4f0e5f75be68840ac",
+    "url": "/assessment/static/media/sample3.c8b11a06.png"
+  },
+  {
+    "revision": "3b1da6e6657c949b04dc1ab9c441dbe7",
+    "url": "/assessment/static/media/sample4.3b1da6e6.png"
+  },
+  {
+    "revision": "129efd685cabb96a485a023958e0d87b",
+    "url": "/assessment/static/media/sample5.129efd68.png"
+  },
+  {
+    "revision": "3f33587f90c120e5bc7836488669c81a",
+    "url": "/assessment/static/media/textpic.3f33587f.jpg"
+  }
+]);
